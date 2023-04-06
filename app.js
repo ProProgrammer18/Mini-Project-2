46cca0c99180773c7492ace6e83c5667effcd1cb
Om Gharge <85841167+ProProgrammer18@users.noreply.github.com>
2023-04-06 19:56:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 app.use(express.json());
 // app.use(express.static());
 
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, "views")));
 
 app.use(express.urlencoded({ extended: true }));
 
